5e68425a9ab76fcf4a9c86939d851f23127ff7ab
frehsegustavo <frehsegustavo@gmail.com>
2023-11-18 09:53:16 -0300

    [mod] Refazer pastas

--- old/gcc-shell/testar-mundo.sh
+++ old/gcc-shell/testar-mundo.sh
@@ -4,5 +4,5 @@ echo "Deletando os arquivo .o e o executavel"
 
 clear;
 cd ..;
-gcc -Wall -o programa programa.c mundo.c entidades.c fila.c principal.c;
+gcc -Wall -o theBoys programa.c mundo.c entidades.c fila.c principal.c;
 cd gcc-shell;
