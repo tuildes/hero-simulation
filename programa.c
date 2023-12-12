be3a0921d460a572ed23f022036f7184e4f41be9
Gustavo Frehse <119139000+tuildes@users.noreply.github.com>
2023-12-12 00:30:27 -0300

    Update programa.c

--- programa.c
+++ programa.c
@@ -3,10 +3,6 @@
     TRABALHO GRANDE - THE BOYS
     ESPECIFICACAÇÂO: https://wiki.inf.ufpr.br/maziero/doku.php?id=c:theboys
     Matéria: Programação 1 - 2023/2
-    Professor: Luiz C. P. Albini
-
-    Aluno: Gustavo Benitez Frehse
-    Matricula: GRR 20235087
     Curso/Semestre: Informática Biomédica (2° semestre)
 
 */
